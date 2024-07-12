155a4c5e1000fa74a4392190699035dba02b0cbf
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-07-12 13:53:50 +0800

    v1.12 commit n push

--- src/main/java/fcu/web/Main.java
+++ src/main/java/fcu/web/Main.java
@@ -1,12 +1,14 @@
 package fcu.web;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 import java.sql.Driver;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args)
@@ -17,13 +19,34 @@ public class Main {
     driver.findElement(By.cssSelector(".usermenu span.login.pl-2")).click();
     driver.findElement(By.cssSelector(".login-form-username input")).sendKeys("d1204433");
     driver.findElement(By.cssSelector(".login-form-password input")).sendKeys("9987321fcuacC");
+
+    try
+    {
+    List<WebElement> elements=driver.findElements(By.cssSelector(""));
+
+    for(WebElement element :elements)
+    {
+    WebElement CourseNameElement=element.findElement(By.cssSelector(""));
+    WebElement TeacherNameElement=element.findElement(By.cssSelector(""));
+
+
+    }
+
+    }
+    catch(NoSuchElementException e)
+    {
+    e.printStackTrace();
+    }
+
+
     driver.findElement(By.cssSelector(".login-form-submit button.btn.btn-primary ")).click();
 
     WebElement selectElement=driver.findElement(By.cssSelector(""));
     Select select=new Select(selectElement);
     select.selectByIndex(0);
 
-    driver.quit();
+
+        driver.quit();
 
 
     }
