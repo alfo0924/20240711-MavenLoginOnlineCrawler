ab1811a27ba784aa90d7dacaf797dca6c3e98ca5
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-07-13 13:58:05 +0800

    v1.488 commit n push

--- src/main/java/fcu/web/Main.java
+++ src/main/java/fcu/web/Main.java
@@ -16,30 +16,27 @@ public class Main {
 
     driver.get("https://ilearn.fcu.edu.tw/");
     driver.findElement(By.cssSelector(".usermenu span.login.pl-2")).click();
-    driver.findElement(By.cssSelector(".login-form-username input")).sendKeys("d1204433");
-    driver.findElement(By.cssSelector(".login-form-password input")).sendKeys("9987321fcuacC");
+    driver.findElement(By.cssSelector(".login-form-username input")).sendKeys("");
+    driver.findElement(By.cssSelector(".login-form-password input")).sendKeys("");
         driver.findElement(By.cssSelector(".login-form-submit button.btn.btn-primary ")).click();
 
         WebElement selectElement =driver.findElement(By.cssSelector("select[name='lmc-filtersemester']"));
         Select select =new Select(selectElement);
         select.selectByIndex(0);
 
-
     try
     {
     List<WebElement> elements = driver.findElements(By.cssSelector("#frontpage-mycurricular "));
     System.out.print("Initiate ");
-//    Thread.sleep(14500);
 
     for(WebElement element : elements)
     {
-        System.out.print("Printin11");
+
     WebElement CourseNameElement = element.findElement(By.cssSelector("h6"));
     WebElement TeacherNameElement = element.findElement(By.cssSelector("div"));
-        System.out.print("Printin22");
-//    System.out.print(CourseNameElement);
+
+    System.out.print(CourseNameElement);
     System.out.print(CourseNameElement.getText());
-//    System.out.print(TeacherNameElement);
     System.out.print(TeacherNameElement.getText());
     }
 
